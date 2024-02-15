e7f7f560d86d325840ee76e1f3dccd234aba21e4
Gözde Madendere <90986708+gozdemadendere@users.noreply.github.com>
2024-02-15 23:50:18 +0300

    Add files via upload

--- 4_Measurement_Problems/AB_Testing/AB_testing.py
+++ 4_Measurement_Problems/AB_Testing/AB_testing.py
@@ -1,156 +1,36 @@
-#####################################################
-# A/B Testing
-#####################################################
+###########################################################################################################
+# A/B TESTING
+###########################################################################################################
 
 # Hipotez testi: Bir inanışı/bir savı test etmek için kullanılan bir istatistiksel analiz yöntemidir.
-# A/B Testing:   2 grup arasında karşılaştırma yapmak için kullanılır. (2 gruba ait ortalama / oran karşılaştırması)
+# A/B Testing:   2 grup arasında bir değişikliğin etkisini ölçmek veya 2 gruba ait ortalama / oran karşılaştırması için kullanılır.
 # Grup karşılaştırmalarında temel amaç, olası farklılıkların şans eseri ortaya çıkıp çıkmadığını test etmektir.
-# Örnek: mobil uygulamada yapılan bir arayüz değişikliıği sonrasında, kullanıcıların uygulamada geçirdikleri günlük ort. süre arttı mı?
 
 
-# A/B Testing (Bağımsız İki Örneklem T Testi) : 2 grup ortalaması arasında karşılaştırma yapmak için kullanılır.
-# A/B Testing (İki Örneklem Oran Testi)       : 2 grup oranları arasında karşılaştırma yapmak için kullanılır.
-# ANOVA (Analysis of Variance)                : 2'den fazla grup ortalamasını karşılaştırmak için kullanılır.
+# 1) A/B Testing (Bağımsız İki Örneklem T Testi) : 2 grup ortalaması arasında karşılaştırma yapmak için kullanılır.
+# 2) A/B Testing (İki Örneklem Oran Testi)       : 2 grup oranları arasında karşılaştırma yapmak için kullanılır.
+# 3) A/B Testing (Ki-Kare Testi)                 : 2 kategorik değişken arasındaki ilişkiyi belirlemek için kullanılır.
+# 4) ANOVA (Analysis of Variance)                : 2'den fazla grup ortalamasını karşılaştırmak için kullanılır.
 
 
+# A/B Testing Örnek:
+# Veri Seti: Bir Web sitesinin eski ve yeni tasarımına ait veriler bulunmaktadır.
+# Gruplar:   Eski tasarım (Kontrol Grubu) ve yeni tasarım (Deney Grubu) olmak üzere 2 farklı grup bulunmaktadır.
 
+# Bağımsız İki Örneklem T Testi: 2 grubun eski ve yeni tasarım altında, geçirdikleri sürelerin ortalama değerlerinin karşılaştırılması
+# Yani bu test, sayısal bir değişken olan "geçirilen süre"yi karşılaştırır.
+# H0: 2 grubun eski ve yeni tasarım altında, geçirdikleri ortalama süre arasında istatistiksel olarak anlamlı bir fark yoktur. (Yani, ortalama süreler hemen hemen aynıdır.)
 
-######################################################
-# Temel İstatistik Kavramları
-######################################################
-
-import itertools
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-
-# !pip install statsmodels
-import statsmodels.stats.api as sms
-from scipy.stats import ttest_1samp, shapiro, levene, ttest_ind, mannwhitneyu, \
-    pearsonr, spearmanr, kendalltau, f_oneway, kruskal
-from statsmodels.stats.proportion import proportions_ztest
-
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', 10)
-pd.set_option("display.width", 500)
-pd.set_option("display.precision", 2)
-# pd.set_option('display.float_format', lambda x: '%.5f' % x)
-
-
-
-############################
-# Sampling (Örneklem)
-############################
-
-# Popülasyon: Bir araştırma veya inceleme yapmak istediğiniz tüm bireylerin veya öğelerin tamamını temsil eder.
-# Örneklem:   Popülasyonun bir alt kümesidir. Popülasyonun temsil ettiği özellikleri veya nitelikleri yansıtmak için seçilir.
-
-populasyon = np.random.randint(0, 80, 10000)   # 0-80 arasında 10.000 adet sayı oluştur
-populasyon.mean() # 39.58
-
-np.random.seed(115)
-
-orneklem = np.random.choice(a=populasyon, size=100)   # popülasyon içinden, 100 adet örneklem seç
-orneklem.mean()  # 39.14
-
-np.random.seed(10)
-orneklem1 = np.random.choice(a=populasyon, size=100)
-orneklem2 = np.random.choice(a=populasyon, size=100)
-orneklem3 = np.random.choice(a=populasyon, size=100)
-orneklem4 = np.random.choice(a=populasyon, size=100)
-orneklem5 = np.random.choice(a=populasyon, size=100)
-orneklem6 = np.random.choice(a=populasyon, size=100)
-orneklem7 = np.random.choice(a=populasyon, size=100)
-orneklem8 = np.random.choice(a=populasyon, size=100)
-orneklem9 = np.random.choice(a=populasyon, size=100)
-orneklem10 = np.random.choice(a=populasyon, size=100)
-
-(orneklem1.mean() + orneklem2.mean() + orneklem3.mean() + orneklem4.mean() + orneklem5.mean()
- + orneklem6.mean() + orneklem7.mean() + orneklem8.mean() + orneklem9.mean() + orneklem10.mean()) / 10    # 40.23
+# Ki-Kare Testi: 2 grubun eski ve yeni tasarım altında, "satın al" düğmesine tıklama oranlarının birbirinden farklı olup olmadığı
+# Yani bu test, kategorik bir değişken olan "satın al" düğmesine tıklama oranlarını karşılaştırır.
+# H0:  İki grup arasında, eski ve yeni tasarım altında, "satın al" düğmesine tıklama oranları arasında istatistiksel olarak anlamlı bir fark yoktur. (Yani, tıklama oranları hemen hemen aynıdır.)
 
 
 
 
-############################
-# Descriptive Statistics (Betimsel İstatistikler)
-############################
-
-# Mean (Ortalama), parametriktir. Median (Medyan), nonparametriktir.
-# Mean ve Median birbirine yakın ise, aykırı değerler yoktur diyebiliriz.
-
-df = sns.load_dataset("tips")
-df.describe().T
-
-
-
-############################
-# Confidence Intervals (Güven Aralıkları)
-############################
-
-# Güven aralıkları, istatistiksel sonuçların güvenilirliğini değerlendirmek ve sonuçların anlamlılığını belirlemek için kullanılır.
-# Örneğin, "95% güven aralığı (50, 70)" ifadesi, popülasyon parametresinin tahmini değerinin %95 olasılıkla 50 ile 70 arasında olduğunu belirtir.
-
-
-# Tips Veri Setindeki Sayısal Değişkenler için Güven Aralığı Hesabı:
-# total_bill sütununda bulunan örnek verilere dayalı olarak, popülasyonun ortalamasının güven aralığını hesaplar.
-sms.DescrStatsW(df["total_bill"]).tconfint_mean()  # 18.66 ve 20.90 arasinda
-
-# tip sütununda bulunan örnek verilere dayalı olarak, popülasyonun ortalamasının güven aralığını hesaplar.
-sms.DescrStatsW(df["tip"]).tconfint_mean()   # 2.82 ve 3.17 arasinda
-
-
-# Titanic Veri Setindeki Sayısal Değişkenler için Güven Aralığı Hesabı
-df = sns.load_dataset("titanic")
-df.describe().T
-
-# age sütununda bulunan örnek verilere dayalı olarak popülasyonun ortalamasının güven aralığını hesaplar. (once age degiskeni icindeki eksik degerleri ucurduk)
-sms.DescrStatsW(df["age"].dropna()).tconfint_mean()   # 28.63 ve 30.76 arasinda
-
-sms.DescrStatsW(df["fare"].dropna()).tconfint_mean()
-
-
-
-######################################################
-# Correlation (Korelasyon)
-######################################################
-
-# 2 değişken arasındaki ilişkinin hem gücünü hem yönünü ifade etmektedir. (-1 ile 1 arasında yer alır)
-# Pozitif korelasyon: bir değişkenin değeri artarken, diğer değişkenin değeri de artar.
-# Negatif korelasyon: bir değişkenin değeri artarken, diğer değişkenin değeri azalır.
-
-
-# Bahşiş veri seti:
-# total_bill: yemeğin toplam fiyatı (bahşiş ve vergi dahil)
-# tip: bahşiş
-# sex: ücreti ödeyen kişinin cinsiyeti (0=male, 1=female)
-# smoker: grupta sigara içen var mı? (0=No, 1=Yes)
-# day: gün (3=Thur, 4=Fri, 5=Sat, 6=Sun)
-# time: ne zaman? (0=Day, 1=Night)
-# size: grupta kaç kişi var?
-
-df = sns.load_dataset('tips')
-df.head()
-
-# Toplam hesaptan, tip i çıkaralım
-df["total_bill"] = df["total_bill"] - df["tip"]
-
-# Toplam hesap arttıkça, bahşiş te artıyor mu?
-# Pozitif yönlü bir korelasyon var gibi duruyor.
-df.plot.scatter("tip", "total_bill")
-plt.show()
-
-# Yorum: Toplam hesap ile bahsis arasinda, orta siddetli, pozitif yönlü bir korelasyon vardir.
-df["tip"].corr(df["total_bill"])
-
-
-
-
-
-
-######################################################
-# A/B Testing (Bağımsız İki Örneklem T Testi)
-######################################################
+############################################################################################################
+# 1) A/B Testing (Bağımsız İki Örneklem T Testi)
+############################################################################################################
 
 # A/B Testing Adımları:
 
@@ -166,15 +46,16 @@ df["tip"].corr(df["total_bill"])
 
 # Notlar:
 # Normallik Varsayımı hem kontrol grubu, hem de test grubu için sağlanmalıdır.
-# Normallik Varsayımı sağlanmıyorsa, direkt mannwhitneyu testini uygularız. Sadece Varyans Homojenliği sağlanmıyorsa, iki örneklem t testi uygulanır ama argüman girilir (equal_var=True).
+# Sadece normallik varsayımı sağlanıyorsa, varyans homojenliği varsayımı sağlanmıyorsa,  düzeltilmiş bağımsız iki örneklem t testi uygularız ama argüman girerek bunu belirtiriz. (Welch'in t-testi, equal_var=True)
 # Argüman girişi şu şekilde: test_stat, pvalue = ttest_ind(df_control['Purchase'], df_test['Purchase'], equal_var=True)
 # Normallik incelemesi öncesi aykırı değer incelemesi ve düzeltmesi yapmak faydalı olabilir.
 
 
 
 
+
 ############################
-# Uygulama 1: Sigara İçenler ve İçmeyenlerin Hesap Ortalamaları Arasında İstatistiksel Olarak Anlamlı bir Fark var mı?
+# Uygulama 1) Sigara İçenler ve İçmeyenlerin Hesap Ortalamaları Arasında İstatistiksel Olarak Anlamlı bir Fark var mı?
 ############################
 
 df = sns.load_dataset("tips")
@@ -261,7 +142,7 @@ print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))   # p-value = 0.
 
 
 ############################
-# Uygulama 2: Titanic Kadın ve Erkek Yolcuların Yaş Ortalamaları Arasında İstatistiksel Olarak Anl. bir Fark. var mıdır?
+# Uygulama 2) Titanic Kadın ve Erkek Yolcuların Yaş Ortalamaları Arasında İstatistiksel Olarak Anl. bir Fark. var mıdır?
 ############################
 
 df = sns.load_dataset("titanic")
@@ -347,7 +228,7 @@ print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))     # p-value = 
 
 
 ############################
-# Uygulama 3: Diyabet Hastası Olan ve Olmayanların Yaş Ort. Arasında İst. Ol. Anl. Fark var mıdır?
+# Uygulama 3) Diyabet Hastası Olan ve Olmayanların Yaş Ort. Arasında İst. Ol. Anl. Fark var mıdır?
 ############################
 
 df = pd.read_csv("datasets/diabetes.csv")
@@ -411,7 +292,7 @@ print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))   # p-value = 0.
 
 
 ###################################################
-# İş Problemi: Kursun Büyük Çoğunluğunu İzleyenler ile İzlemeyenlerin Puanları Birbirinden Farklı mı?
+# Uygulama 4) İş Problemi: Kursun Büyük Çoğunluğunu İzleyenler ile İzlemeyenlerin Puanları Birbirinden Farklı mı?
 ###################################################
 
 df = pd.read_csv("datasets/course_reviews.csv")
@@ -471,9 +352,9 @@ print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))      # p-value =
 
 
 
-######################################################
-# AB Testing (İki Örneklem Oran Testi)
-######################################################
+############################################################################################################
+# 2) A/B Testing (İki Örneklem Oran Testi)
+############################################################################################################
 
 ############################
 # 1) Hipotezlerin Tanımlanması
@@ -518,9 +399,9 @@ print('Test Stat = %.4f, p-value = %.4f' % (test_stat, pvalue))
 
 
 
-######################################################
-# ANOVA (Analysis of Variance)
-######################################################
+############################################################################################################
+# 4) ANOVA (Analysis of Variance)
+############################################################################################################
 
 # İkiden fazla grup ortalamasını karşılaştırmak için kullanılır.
 
